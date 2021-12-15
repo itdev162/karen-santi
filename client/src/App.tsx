3ee694d3156ac12cafac85028de305746457d309
santiak3 <71164211+santiak3@users.noreply.github.com>
2021-12-15 16:23:10 +0000

    Completed Activity 9

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -30,10 +30,24 @@ class App extends React.Component {
     });
   }
 
+  deletePost = post => {
+    axios
+      .delete(`http://localhost:5000/api/posts/${post.id}`)
+      .then(response => {
+        const newPosts = this.state.posts.filter(p => p.id !== post.id);
+        this.setState({
+          posts: [...newPosts]
+        });
+      })
+      .catch(error => {
+        console.error(`Error deleting post: ${error}`);
+      });
+  };
+
   render() {
     const { posts, post } = this.state;
 
-    return(
+  return(
       <Router>
         <div className="App">
           <header className="App=header">
@@ -42,7 +56,11 @@ class App extends React.Component {
           <main className="App-content">
             <Switch>
               <Route exact path="/">
-                <PostList posts={posts} clickPost={this.viewPost} />
+                <PostList
+                 posts={posts} 
+                 clickPost={this.viewPost}
+                 deletePost={this.deletePost}
+                  />
               </Route>
               <Route path="/posts/:postId">
                 <Post post={post} />
